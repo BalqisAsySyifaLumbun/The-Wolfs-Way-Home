972d5cf3a5c3943127d78bbf74415482c9ad12e4
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2025-05-28 11:13:15 +0700

    [FEAT] FIX Hint Feature

--- scripts/input_computer.gd
+++ scripts/input_computer.gd
@@ -10,20 +10,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	#Implementasi Hint dengan menekan tombol H
 	if Input.is_key_pressed(KEY_H):
-		if on_press == false:
-			on_press = true
-			$RichTextLabel.visible = true
-			$RichTextLabel.modulate = Color(0,0,0,0.5)
-			if Global.access_pass == false:
-				$RichTextLabel.text = "The Password is Access"
-			else:
-				if Global.met_jackalope == true:
-					$RichTextLabel.text = "Imagine 'pleading to humans to abort ones' mining permit', haha definitely not us"
-				else:
-					$RichTextLabel.text = "I'm only in your imagination, but what sentence does sound like 'Pleaseee, abort our mine permit' or something like that?"
+		$RichTextLabel.visible = true
+		$RichTextLabel.modulate = Color(0,0,0,0.5)
+		if Global.access_pass == false:
+			$RichTextLabel.text = "The Password is Access"
 		else:
-			on_press = false
 			$RichTextLabel.visible = false
+			$RichTextLabel2.modulate = Color(0,0,0,0.5)
+			$RichTextLabel2.visible = true
+			if Global.met_jackalope == true:
+				$RichTextLabel2.text = "Imagine 'pleading to humans to abort ones' mining permit', haha definitely not us"
+			else:
+				$RichTextLabel2.text = "I'm only in your imagination, but what sentence does sound like 'Pleaseee, abort our mine permit' or something like that?"
 			
 func _input_event(viewport, event, shape_idx):
 	# Check if it's a mouse button event (left click)
@@ -37,9 +35,9 @@ func _input_event(viewport, event, shape_idx):
 					Dialogic.start("cutscene_wrong_2")
 					if Global.incorrect_password >=1:
 						$RichTextLabel.visible = true
-						$Hint_Area.visible = true
 				else:
 					Global.access_pass = true
+					$RichTextLabel.modulate = Color(0,0,0,0.0)
 					$TextEdit.visible = false
 					$TextEdit2.visible = true
 					$TextEdit4.visible = true
@@ -51,22 +49,28 @@ func _input_event(viewport, event, shape_idx):
 					
 					if $TextEdit2.get_text().to_upper() != 'PLEASE' and $TextEdit2.get_text().to_upper() != 'FLEAS':
 						Dialogic.start("wrong")
+						$RichTextLabel2.visible = true
 					
 					if Global.met_jackalope == false:
 						if $TextEdit3.get_text().to_upper() != 'A':
 							Dialogic.start("wrong")
+							$RichTextLabel2.visible = true
 					
 					if $TextEdit4.get_text().to_upper() != 'BOARD' and $TextEdit4.get_text().to_upper() != 'ABORT':
 						Dialogic.start("wrong")
+						$RichTextLabel2.visible = true
 					
 					if $TextEdit5.get_text().to_upper() != 'HOUR' and $TextEdit5.get_text().to_upper() != 'OUR':
 						Dialogic.start("wrong")
+						$RichTextLabel2.visible = true
 					
 					if $TextEdit6.get_text().to_upper() != 'MINE':
 							Dialogic.start("wrong")
+							$RichTextLabel.visible = true
 							
 					if $TextEdit7.get_text().to_upper() != 'PERMIT':
 							Dialogic.start("wrong")
+							$RichTextLabel2.visible = true
 					
 					else:
 						$TextEdit2.visible = false
@@ -77,13 +81,13 @@ func _input_event(viewport, event, shape_idx):
 						$TextEdit3.visible = false
 						if Global.met_jackalope == false:
 							$AnimatedSprite2D.animation = "no_dict"
-							$RichTextLabel.visible = false
+							$RichTextLabel2.visible = false
 							Dialogic.start("finish_good")
 							await Dialogic.timeline_ended
 							$AnimatedSprite2D.animation = "dict"
 						if Global.met_jackalope == true:
 							$AnimatedSprite2D.animation = "dict"
-							$RichTextLabel.visible = false
+							$RichTextLabel2.visible = false
 							Dialogic.start("finish_great")
 							await Dialogic.timeline_ended
 							$AnimatedSprite2D.animation = "new_animation"
